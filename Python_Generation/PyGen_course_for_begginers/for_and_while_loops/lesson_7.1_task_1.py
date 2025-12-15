cfc1955ba8753806612831e6784d8fd5d15a471f
ADDrey <andalexs@yandex.ru>
2025-12-15 19:38:59 +0300

    Complete "loop for" blok from loops module

---
+++ Python_Generation/PyGen_course_for_begginers/for_and_while_loops/lesson_7.1_task_1.py
@@ -0,0 +1,29 @@
+# Python is awesome 🐍
+# 
+# Напишите программу, которая выводит текст «Python is awesome!» (без кавычек) 1010 раз.
+# 
+# Формат входных данных
+# На вход программе ничего не подаётся.
+# 
+# Формат выходных данных
+# Программа должна вывести 1010 раз текст «Python is awesome!» (без кавычек), каждый на отдельной строке.
+# 
+# 
+# Sample Input:
+# 
+# Sample Output:
+# 
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+# Python is awesome!
+
+for i in range(10):
+    print('Python is awesome!')
+
